472f6ebfe5bd94c584a7e3c61570ad7baf489875
deathrashed <rileydevoy@hotmail.com>
2026-07-25 17:53:31 +1000

    chore: update icon files and build artifacts

--- color/music/rings/Tool/GPT/src/RingIcon/obj/Release/net10.0/RingIcon.AssemblyInfo.cs
+++ color/music/rings/Tool/GPT/src/RingIcon/obj/Release/net10.0/RingIcon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RingIcon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804a642289b68cba5c05f35a23af0e757a2498dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164ca559a3dc24340323cca47b0cf997ccbe9c2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RingIcon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RingIcon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
